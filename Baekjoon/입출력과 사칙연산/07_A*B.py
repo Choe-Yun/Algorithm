ad021083a98b54fb2189f8151701a05bf2a9c9eb
Choe-Yun <cjh7687@gmail.com>
2021-05-12 10:37:27 +0900

    사칙연산

---
+++ Baekjoon/입출력과 사칙연산/07_A*B.py
@@ -0,0 +1,20 @@
+## 문제
+
+# 두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+
+## 입력
+
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+
+## 출력
+
+# 첫째 줄에 A*B를 출력한다.
+
+
+input_num = input().split(" ")
+
+A = int(input_num[0])
+B = int(input_num[1])
+
+print(A*B)
