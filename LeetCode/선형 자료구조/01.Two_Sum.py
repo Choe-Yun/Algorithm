4ef01f43ec413488595943a906475afd11fdc160
Choe-Yun <cjh7687@gmail.com>
2021-05-22 23:25:01 +0900

    선형 자료구조

---
+++ LeetCode/선형 자료구조/01.Two_Sum.py
@@ -0,0 +1,34 @@
+### 문제
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+
+
+### Example
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
+
+
+##### code
+
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+
+        # 하나의 `for`문으로 통합
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
