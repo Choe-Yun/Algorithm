2e4228e8fdda085fbac1fcac317bc44372ce4055
Choe-Yun <cjh7687@gmail.com>
2021-05-23 07:23:43 +0900

    선형 자료구조

---
+++ LeetCode/선형 자료구조/02.Trapping_Rain_Water.py
@@ -0,0 +1,72 @@
+### 문제
+
+# Given n non-negative integers representing an elevation map where the width of each bar is 1,
+# compute how much water it can trap after raining.
+
+
+### Example
+
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+# In this case, 6 units of rain water (blue section) are being trapped.
+
+
+
+
+
+###### code
+
+# 투 포인터를 최대 이동
+
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            # 더 높은 쪽을 향해 투 포인터 이동
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+
+# 스택을 이용
+
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 꺼낸다
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
+        return volume
