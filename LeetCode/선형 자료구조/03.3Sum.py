546c89453d061cc48634abf195b420a494a9528f
Choe-Yun <cjh7687@gmail.com>
2021-05-24 22:57:00 +0900

    선형 자료구조

---
+++ LeetCode/선형 자료구조/03.3Sum.py
@@ -0,0 +1,48 @@
+### 문제
+
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+#
+# Notice that the solution set must not contain duplicate triplets.
+
+
+### Example 1
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+
+##### code
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            # 중복된 값 건너뛰기
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # 간격을 좁혀가며 합 `sum` 계산
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    # `sum = 0`인 경우이므로 정답 및 스킵 처리
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
+        return results
